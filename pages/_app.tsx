f4422c01d3e812df9421e8c8446193ce0fcf0f1e
Adivia Gilang Prakarsa <adivia.gilang@onebyone.com.sg>
2022-04-03 15:57:50 +0700

    feat: layout books

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.css';
 import type { AppContext, AppInitialProps, AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppContext & AppInitialProps) {
   return (
     <Provider store={store}>
+      <Header />
       <Component {...pageProps} />
     </Provider>
   );
