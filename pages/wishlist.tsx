39239b4fba768f016a000a5ecc62312515133a9a
Adivia Gilang Prakarsa <adivia.gilang@onebyone.com.sg>
2022-04-04 12:45:42 +0700

    fix: remove input

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -42,24 +42,6 @@ const WishlistPage: NextPage = () => {
       </Head>
 
       <main>
-        <div className="container mx-auto p-10">
-          <div className="flex items-center justify-center">
-            <div className="flex border-2 border-gray-200 rounded w-full">
-              <input
-                type="text"
-                className="px-4 py-2 w-full"
-                placeholder="Search..."
-                onChange={(e) => setInputSearch(e.target.value)}
-              />
-              <button
-                onClick={handleSubmit}
-                className="px-4 text-white bg-gray-600 border-l "
-              >
-                Search
-              </button>
-            </div>
-          </div>
-        </div>
         <div className="container mx-auto flex">
           {/* <Sidebar /> */}
           <Content data={wishlists} cardType="wishlist" />
